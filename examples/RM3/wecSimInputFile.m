1eb9cd3ce961418e7d0eadc95fb6ee60b25679b5
Husain <shusain@nrel.gov>
2022-10-06 17:26:14 -0600

    colorUpdate

--- examples/RM3/wecSimInputFile.m
+++ examples/RM3/wecSimInputFile.m
@@ -9,9 +9,7 @@ simu.endTime = 500;                     % Simulation End Time [s]
 simu.solver = 'ode4';                   % simu.solver = 'ode4' for fixed step & simu.solver = 'ode45' for variable step 
 simu.dt = 0.01; 							% Simulation time-step [s]
 
-%% Wave Information 
-
-
+%% Wave Information
 bemFreq = 0.02:0.02:5.2;
 bemWaterDepth = 100;
 
@@ -20,95 +18,79 @@ waves = waveClass('irregular');       % Initialize Wave Class and Specify Type
 waves.height = 0.0;                     % Wave Height [m]
 waves.period = 3; 
 waves.spectrumType = 'PM';
+waves.direction = 90;
+waves.spread = 1;
+% 
+% marker = 50;
+% distance = 20;
+% [X,Y] = meshgrid(-marker:distance:marker,-marker:distance:marker);
+% waves.marker.location = [reshape(X,[],1),reshape(Y,[],1)]; % Marker Locations [X,Y]
+% clear('marker','distance','X','Y')
+% waves.marker.style = 3; % 1: Sphere, 2: Cube, 3: Frame.
+% waves.marker.size = 15; % Marker Size in Pixels
+
+
 % % Regular Waves  
 waves1 = waveClass('irregular');           % Initialize Wave Class and Specify Type                                 
 waves1.height = 5.0;                     % Wave Height [m]
 waves1.period = 2;                       % Wave Period [s]
 waves1.spectrumType = 'PM';
+waves1.direction = 90;
+waves1.spread = 1;
+% 
+% marker = 75;
+% distance = 10;
+% [X,Y] = meshgrid(-marker:distance:marker,-marker:distance:marker);
+% waves1.marker.location = [reshape(X,[],1),reshape(Y,[],1)]; % Marker Locations [X,Y]
+% clear('marker','distance','X','Y')
+% waves1.marker.style = 2; % 1: Sphere, 2: Cube, 3: Frame.
+% waves1.marker.size = 20; % Marker Size in Pixels
 waveGen(waves1,simu,bemFreq,bemWaterDepth) 
 w1 = waves1.waveAmpTime;
 
 
 waves2 = waveClass('irregular');           % Initialize Wave Class and Specify Type                                 
-waves2.height = 0.25;                     % Wave Height [m]
-waves2.period = 4;                       % Wave Period [s]
+waves2.height = 5;                     % Wave Height [m]
+waves2.period = 2;                       % Wave Period [s]
 waves2.spectrumType = 'PM';
+waves2.direction = 0;
+waves2.spread = 1;
 
-
+% marker = 50;
+% distance = 20;
+% [X,Y] = meshgrid(-marker:distance:marker,-marker:distance:marker);
+% waves2.marker.location = [reshape(X,[],1),reshape(Y,[],1)]; % Marker Locations [X,Y]
+% clear('marker','distance','X','Y')
+% waves2.marker.style = 1; % 1: Sphere, 2: Cube, 3: Frame.
+% waves2.marker.size = 30; % Marker Size in Pixels
 waveGen(waves2,simu,bemFreq,bemWaterDepth) 
 w2 = waves2.waveAmpTime;
 
+% waves3 = waveClass('irregular');           % Initialize Wave Class and Specify Type                                 
+% waves3.height = .75;                     % Wave Height [m]
+% waves3.period = 2;                       % Wave Period [s]
+% waves3.spectrumType = 'PM';
+% 
+% 
+% waveGen(waves3,simu,bemFreq,bemWaterDepth) 
+% w3 = waves3.waveAmpTime;
+% 
+% waves4 = waveClass('irregular');           % Initialize Wave Class and Specify Type                                 
+% waves4.height = 2;                     % Wave Height [m]
+% waves4.period = 5;                       % Wave Period [s]
+% waves4.spectrumType = 'PM';
+% 
+% 
+% waveGen(waves4,simu,bemFreq,bemWaterDepth) 
+% w4 = waves4.waveAmpTime;
 
-waves3 = waveClass('irregular');           % Initialize Wave Class and Specify Type                                 
-waves3.height = .75;                     % Wave Height [m]
-waves3.period = 2;                       % Wave Period [s]
-waves3.spectrumType = 'PM';
-
-
-waveGen(waves3,simu,bemFreq,bemWaterDepth) 
-w3 = waves3.waveAmpTime;
-
-waves4 = waveClass('irregular');           % Initialize Wave Class and Specify Type                                 
-waves4.height = 2;                     % Wave Height [m]
-waves4.period = 5;                       % Wave Period [s]
-waves4.spectrumType = 'PM';
-
-
-waveGen(waves4,simu,bemFreq,bemWaterDepth) 
-w4 = waves4.waveAmpTime;
-
-waveGroup = [waves1;waves2;waves3;waves4];
+waveGroup = [waves1;waves2];%;waves3;waves4];
 SwellandChop(:,1) = w1(:,1); 
-SwellandChop(:,2) = w1(:,2) + w2(:,2) + w3(:,2) + w4(:,2);
+SwellandChop(:,2) = w1(:,2) + w2(:,2);% + w3(:,2) + w4(:,2);
 
 
 
 save('SwellandChop.mat','SwellandChop')
-
-% 
-% waves = waveClass('spectrumImport');           % Initialize Wave Class and Specify Type                                 
-% waves.spectrumFile ='SwellandChop.mat';
-% load(waves.spectrumFile);
-% waveElevUser(waves,simu.rampTime,length(simu.time)-1,SwellandChop,simu.time)
-
-
-
-% % Regular Waves with CIC
-% waves = waveClass('regularCIC');          % Initialize Wave Class and Specify Type                                 
-% waves.height = 2.5;                       % Wave Height [m]
-% waves.period = 8;                         % Wave Period [s]
-
-% % Irregular Waves using PM Spectrum 
-% waves = waveClass('irregular');           % Initialize Wave Class and Specify Type
-% waves.height = 2.5;                       % Significant Wave Height [m]
-% waves.period = 8;                         % Peak Period [s]
-% waves.spectrumType = 'PM';                % Specify Wave Spectrum Type
-
-
-
-% % Irregular Waves using JS Spectrum with Equal Energy and Seeded Phase
-% waves = waveClass('irregular');           % Initialize Wave Class and Specify Type
-% waves.height = 2.5;                       % Significant Wave Height [m]
-% waves.period = 8;                         % Peak Period [s]
-% waves.spectrumType = 'JS';                % Specify Wave Spectrum Type
-% waves.bem.option = 'EqualEnergy';         % Uses 'EqualEnergy' bins (default) 
-% waves.phaseSeed = 1;                      % Phase is seeded so eta is the same
-
-% % Irregular Waves using PM Spectrum with Traditional and State Space 
-% waves = waveClass('irregular');           % Initialize Wave Class and Specify Type
-% waves.height = 2.5;                       % Significant Wave Height [m]
-% waves.period = 8;                         % Peak Period [s]
-% waves.spectrumType = 'PM';                % Specify Wave Spectrum Type
-% simu.stateSpace = 1;                      % Turn on State Space
-% waves.bem.option = 'Traditional';         % Uses 1000 frequnecies
-
-% % Irregular Waves with imported spectrum
-% waves = waveClass('spectrumImport');      % Create the Wave Variable and Specify Type
-% waves.spectrumFile = 'spectrumData.mat';  % Name of User-Defined Spectrum File [:,2] = [f, Sf]
-
-% % Waves with imported wave elevation time-history  
-% waves = waveClass('elevationImport');          % Create the Wave Variable and Specify Type
-% waves.elevationFile = 'elevationData.mat';     % Name of User-Defined Time-Series File [:,2] = [time, eta]
 
 
 %% Body Data
@@ -121,13 +103,16 @@ body(1).mass = 'equilibrium';
     % Body Mass. The 'equilibrium' Option Sets it to the Displaced Water 
     % Weight.
 body(1).inertia = [20907301 21306090.66 37085481.11];  % Moment of Inertia [kg*m^2]     
+body(1).yaw.option=1;                           % Turn passive yaw ON
+body(1).yaw.threshold=0.01;
 
 % Spar/Plate
 body(2) = bodyClass('two_headings/rm3.h5'); 
 body(2).geometryFile = 'geometry/plate.stl'; 
 body(2).mass = 'equilibrium';                   
 body(2).inertia = [94419614.57 94407091.24 28542224.82];
-
+body(2).yaw.option=1;                           % Turn passive yaw ON
+body(2).yaw.threshold=0.01; 
 
 
 %% PTO and Constraint Parameters
